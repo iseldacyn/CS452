53108134e3097b4af2c72e628ec8b10d5c257799
iselda aiello <aielloise@gmail.com>
2023-12-13 04:26:28 -0500

    update .obj to .js and new lamp model

--- proj2/obj/obj2js.py
+++ proj2/obj/obj2js.py
@@ -9,107 +9,136 @@ oF = open( oF, "w" )
 n = 0
 v = 0
 
-with open( iF, "r" ) as file:
+for i in range(4):
+    file = open( iF, "r" )
     lines = file.readlines()
-    for line in lines:
-        line = line.split()
-        if not line:
-            continue
+    if i == 0:
+        oF.write( "function getVertices()\n{\n" )
+        for line in lines:
+            line = line.split()
+            if not line:
+                continue
+            if line[0] != 'v':
+                continue
 
-        if n == 0 and line[0] == 'v':
-            oF.write( "function getVertices()\n{\n" )
-            oF.write( f'\tvar vertices = [ vec4( {line[1]}, {line[2]}, {line[3]}, 1.0 )' )
-            v += 1
-            n += 1
+            if n == 0:
+                oF.write( f'\tvar vertices = [ vec4( {line[1]}, {line[2]}, {line[3]}, 1.0 )' )
+                v += 1
+                n += 1
 
-        elif line[0] == 'v':
-            oF.write( f',\n\t\t\t\tvec4( {line[1]}, {line[2]}, {line[3]}, 1.0 )' )
-            v += 1
-
-        if n == 1 and line[0] == 'vt':
-            oF.write( "];\n\n\treturn vertices;\n}\n\nfunction getTextureCoordinates()\n{\n" )
-            oF.write( f'\tvar textureCoordinates = [ vec2( {line[1]}, {line[2]} )' )
-            n += 1
-
-        elif line[0] == 'vt':
-            oF.write( f',\n\t\t\t\tvec2( {line[1]}, {line[2]} )' )
-
-        if n == 2 and line[0] == 'vn':
-            oF.write( "];\n\n\treturn textureCoordinates;\n}\n\nfunction getVertexNormals()\n{\n" )
-            oF.write( f'\tvar vertexNormals = [ vec3( {line[1]}, {line[2]}, {line[3]} )' )
-            n += 1
-        
-        elif line[0] == 'vn':
-            oF.write( f',\n\t\t\t\tvec3( {line[1]}, {line[2]}, {line[3]} )' )
-
-        if n == 3 and line[0] == 'f':
-            oF.write( "];\n\n\treturn vertexNormals;\n}\n\nfunction getFaces()\n{\n" )
-            oF.write( f'\n\tvar indexList = [ ' )
-            indices = []
-            for index in line:
-                index = index.split('/')
-                indices.append( index )
-            if int(indices[1][0]) < 0:
-                oF.write( f'{v-indices[1][0]}' )
             else:
-                oF.write( f'{indices[1][0]}' )
-            if int(indices[2][0]) < 0:
-                oF.write( f', {v-indices[2][0]}' )
+                oF.write( f',\n\t\t\t\tvec4( {line[1]}, {line[2]}, {line[3]}, 1.0 )' )
+                v += 1
+
+    if i == 1:
+        oF.write( "];\n\n\treturn vertices;\n}\n\nfunction getTextureCoordinates()\n{\n" )
+        for line in lines:
+            line = line.split()
+            if not line:
+                continue
+            if line[0] != 'vt':
+                continue
+
+            if n == 1:
+                oF.write( f'\tvar textureCoordinates = [ vec2( {line[1]}, {line[2]} )' )
+                n += 1
+
             else:
-                oF.write( f', {indices[2][0]}' )
-            if int(indices[3][0]) < 0:
-                oF.write( f', {v-indices[3][0]}' )
+                oF.write( f',\n\t\t\t\tvec2( {line[1]}, {line[2]} )' )
+
+    if i == 2:
+        oF.write( "];\n\n\treturn textureCoordinates;\n}\n\nfunction getVertexNormals()\n{\n" )
+        for line in lines:
+            line = line.split()
+            if not line:
+                continue
+            if line[0] != 'vn':
+                continue
+
+            if n == 2:
+                oF.write( f'\tvar vertexNormals = [ vec3( {line[1]}, {line[2]}, {line[3]} )' )
+                n += 1
+            
             else:
-                oF.write( f', {indices[3][0]}' )
-            i = len(indices)-1
-            while ( i > 3 ):
+                oF.write( f',\n\t\t\t\tvec3( {line[1]}, {line[2]}, {line[3]} )' )
+
+    if i == 3:
+        oF.write( "];\n\n\treturn vertexNormals;\n}\n\nfunction getFaces()\n{\n" )
+        for line in lines:
+            line = line.split()
+            if not line:
+                continue
+            if line[0] != 'f':
+                continue
+
+            if n == 3:
+                oF.write( f'\n\tvar indexList = [ ' )
+                indices = []
+                for index in line:
+                    index = index.split('/')
+                    indices.append( index )
                 if int(indices[1][0]) < 0:
-                    oF.write( f',\n\t\t\t\t{v-indices[1][0]}' )
+                    oF.write( f'{v-int(indices[1][0])-1}' )
                 else:
-                    oF.write( f',\n\t\t\t\t{indices[1][0]}' )
-                if int(indices[i-1][0]) < 0:
-                    oF.write( f', {v-indices[i-1][0]}' )
+                    oF.write( f'{int(indices[1][0])-1}' )
+                if int(indices[2][0]) < 0:
+                    oF.write( f', {v-int(indices[2][0])-1}' )
                 else:
-                    oF.write( f', {indices[i-1][0]}' )
-                if int(indices[i][0]) < 0:
-                    oF.write( f', {v-indices[i][0]}' )
+                    oF.write( f', {int(indices[2][0])-1}' )
+                if int(indices[3][0]) < 0:
+                    oF.write( f', {v-int(indices[3][0])-1}' )
                 else:
-                    oF.write( f', {indices[i][0]}' )
-                i -= 1
-            n += 1
+                    oF.write( f', {int(indices[3][0])-1}' )
+                i = len(indices)-1
+                while ( i > 3 ):
+                    if int(indices[1][0]) < 0:
+                        oF.write( f',\n\t\t\t\t{v-int(indices[1][0])-1}' )
+                    else:
+                        oF.write( f',\n\t\t\t\t{int(indices[1][0])-1}' )
+                    if int(indices[i-1][0]) < 0:
+                        oF.write( f', {v-int(indices[i-1][0])-1}' )
+                    else:
+                        oF.write( f', {int(indices[i-1][0])-1}' )
+                    if int(indices[i][0]) < 0:
+                        oF.write( f', {v-int(indices[i][0])-1}' )
+                    else:
+                        oF.write( f', {int(indices[i][0])-1}' )
+                    i -= 1
+                n += 1
 
-        if line[0] == 'f':
-            indices = []
-            for index in line:
-                index = index.split('/')
-                indices.append( index )
-            if int(indices[1][0]) < 0:
-                oF.write( f',\n\t\t\t\t{v-indices[1][0]}' )
             else:
-                oF.write( f',\n\t\t\t\t{indices[1][0]}' )
-            if int(indices[2][0]) < 0:
-                oF.write( f', {v-indices[2][0]}' )
-            else:
-                oF.write( f', {indices[2][0]}' )
-            if int(indices[3][0]) < 0:
-                oF.write( f', {v-indices[3][0]}' )
-            else:
-                oF.write( f', {indices[3][0]}' )
-            i = len(indices)-1
-            while ( i > 3 ):
+                indices = []
+                for index in line:
+                    index = index.split('/')
+                    indices.append( index )
                 if int(indices[1][0]) < 0:
-                    oF.write( f',\n\t\t\t\t{v-indices[1][0]}' )
+                    oF.write( f',\n\t\t\t\t{v-int(indices[1][0])-1}' )
                 else:
-                    oF.write( f',\n\t\t\t\t{indices[1][0]}' )
-                if int(indices[i-1][0]) < 0:
-                    oF.write( f', {v-indices[i-1][0]}' )
+                    oF.write( f',\n\t\t\t\t{int(indices[1][0])-1}' )
+                if int(indices[1][0]) < 0:
+                    oF.write( f', {v-int(indices[2][0])-1}' )
                 else:
-                    oF.write( f', {indices[i-1][0]}' )
-                if int(indices[i][0]) < 0:
-                    oF.write( f', {v-indices[i][0]}' )
+                    oF.write( f', {int(indices[2][0])-1}' )
+                if int(indices[2][0]) < 0:
+                    oF.write( f', {v-int(indices[3][0])-1}' )
                 else:
-                    oF.write( f', {indices[i][0]}' )
-                i -= 1
+                    oF.write( f', {int(indices[3][0])-1}' )
+                i = len(indices)-1
+                while ( i > 3 ):
+                    if int(indices[1][0]) < 0:
+                        oF.write( f',\n\t\t\t\t{v-int(indices[1][0])-1}' )
+                    else:
+                        oF.write( f',\n\t\t\t\t{int(indices[1][0])-1}' )
+                    if int(indices[i-1][0]) < 0:
+                        oF.write( f', {v-int(indices[i-1][0])-1}' )
+                    else:
+                        oF.write( f', {int(indices[i-1][0])-1}' )
+                    if int(indices[i][0]) < 0:
+                        oF.write( f', {v-int(indices[i][0])-1}' )
+                    else:
+                        oF.write( f', {int(indices[i][0])-1}' )
+                    i -= 1
+    file.close()
 
-    oF.write( " ];\n\n\treturn indexList;\n}" )
-    print( "File written to " + oFname )
+oF.write( " ];\n\n\treturn indexList;\n}" )
+print( "File written to " + oFname )
